8d66c414642ddd00ca99a6088da509300191fcc2
Esdras Gois <esdras.gois-ext@ab-inbev.com>
2023-11-14 16:15:25 -0300

    Initial Commit

---
+++ pokedex/pokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  pokedexApp.swift
+//  pokedex
+//
+//  Created by Esdras Santos Gois on 14/11/23.
+//
+
+import SwiftUI
+
+@main
+struct pokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
